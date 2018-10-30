a6210c758b6211c076802ba6bdea674183d8f147
Ansion13 <zhushengnan13@163.com>
2018-10-30 11:59:52 +0800

    Update dish_getbypage.php

--- data/dish_getbypage.php
+++ data/dish_getbypage.php
@@ -13,7 +13,7 @@ if(empty($start))
     $start = 0;
 }
 
-$conn = mysqli_connect('127.0.0.1','root','','kaifanla');
+$conn = mysqli_connect('https://github.com/Ansion13/kaifanla.git','root','','kaifanla');
 $sql = "SET NAMES UTF8";
 mysqli_query($conn,$sql);
 
